8eef3e242e2e4178401642df193eb8c3cb5cd51c
ppepin34 <ppepin34@gmail.com>
2022-06-24 20:42:20 -0400

    pizza model created

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+// import routes
 const htmlRoutes = require('./html/html-routes');
+const apiRoutes = require('./api');
 
+// add prefix of `/api` to api routes
+router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
